278ac077019d2c735b57f9eaa830c3c7b712ccd2
thang2710 <vuthanh27102004@gmail.com>
2024-11-29 15:31:05 +0700

    fix loi pong farm

--- src/main/java/com/laptrinhweb/demo/controller/HomeController.java
+++ src/main/java/com/laptrinhweb/demo/controller/HomeController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String index() {
-        return index();
+        return "index";
     }
     
 }
