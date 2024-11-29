ae6896d651f09241f901cdcfee11a8fad2ae82ad
thang2710 <vuthanh27102004@gmail.com>
2024-11-29 13:50:02 +0700

    first commit

---
+++ src/main/java/com/laptrinhweb/demo/controller/BlogController.java
@@ -0,0 +1,12 @@
+package com.laptrinhweb.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BlogController {
+    @RequestMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+}
