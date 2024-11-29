824fb3005bc4515d72174bc4ccdae46ea942d262
thang2710 <vuthanh27102004@gmail.com>
2024-11-29 15:43:21 +0700

    update 2

--- src/main/java/com/laptrinhweb/demo/controller/HomeController.java
+++ src/main/java/com/laptrinhweb/demo/controller/HomeController.java
@@ -1,9 +1,9 @@
 package com.laptrinhweb.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
     @GetMapping("/home")
     public String index() {
