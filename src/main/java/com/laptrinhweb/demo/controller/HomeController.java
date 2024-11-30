3ee8a57a42dc6afed83fbcfe163e3a7e03e070cb
thang2710 <vuthanh27102004@gmail.com>
2024-11-30 22:32:56 +0700

    fragment

--- src/main/java/com/laptrinhweb/demo/controller/HomeController.java
+++ src/main/java/com/laptrinhweb/demo/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.laptrinhweb.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class HomeController {
 @RequestMapping("/home")
@@ -9,4 +11,4 @@ public class HomeController {
         return " index.html() ";
     }
 }
-//hellomaycung
+
